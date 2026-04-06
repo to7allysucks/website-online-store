40b5ec6a64e7b5fd4d1b4c6216849ab002d7064f
to7allysucks <to7allysucks@gmail.com>
2026-04-06 10:28:29 +0300

    feat: add config file

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-@app.get('/health', summary='Health')
+@app.get('/health', summary='Health', tags=['Root'])
 def health():
     return {"success": "OK"}
 
